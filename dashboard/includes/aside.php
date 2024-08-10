fa44dda584f81df2ca047f249864539f2127543c
ultimate-yasser <yasser75894@gmail.com>
2024-08-10 17:34:36 +0300

    update aside.php

--- dashboard/includes/aside.php
+++ dashboard/includes/aside.php
@@ -5,19 +5,19 @@
         <nav class="sidebar-nav">
             <ul id="sidebarnav" class="pt-4">
                 <li class="sidebar-item">
-                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="#" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Home</span></a>
+                    <a class="sidebar-link waves-effect waves-dark sidebar-link" href="../../index.php" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Home</span></a>
                 </li>
                 <li class="sidebar-item">
                     <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-receipt"></i><span class="hide-menu">Dashboards</span></a>
                     <ul aria-expanded="false" class="collapse first-level">
                         <li class="sidebar-item">
-                            <a href="#" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu">Employees</span></a>
+                            <a href="../employees/index.php" class="sidebar-link"><i class="mdi mdi-note-outline"></i><span class="hide-menu">Employees</span></a>
                         </li>
                         <li class="sidebar-item">
-                            <a href="#" class="sidebar-link"><i class="mdi mdi-note-plus"></i><span class="hide-menu">Offices</span></a>
+                            <a href="../offices/index.php" class="sidebar-link"><i class="mdi mdi-note-plus"></i><span class="hide-menu">Offices</span></a>
                         </li>
                         <li class="sidebar-item">
-                            <a href="#" class="sidebar-link"><i class="mdi mdi-note-plus"></i><span class="hide-menu">Product Lines</span></a>
+                            <a href="../products/index.php" class="sidebar-link"><i class="mdi mdi-note-plus"></i><span class="hide-menu">Products</span></a>
                         </li>
                     </ul>
                 </li>
